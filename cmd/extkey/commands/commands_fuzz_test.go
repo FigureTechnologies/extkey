fad2891e3fa8ec8de835621122e0d3a91c78389c
wbaker-figure <85513003+wbaker-figure@users.noreply.github.com>
2022-09-13 10:27:30 -0700

    adding tests (#5)

---
+++ cmd/extkey/commands/commands_fuzz_test.go
@@ -0,0 +1,49 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"strings"
+	"testing"
+)
+
+// Golang doesn't support multi fuzz test cases. Aggregated in order to save time fuzzing
+func FuzzCommands(f *testing.F) {
+	f.Fuzz(func(t *testing.T, hrp string, path string, seed string, xkey string) {
+		paths := []string{path}
+		var err error
+		var formatter Formatter
+		formatter, err = formatize(strings.TrimSpace("json"))
+		if err != nil {
+			t.Error(err)
+		} else {
+			// Fuzz test generate and encode functions with fuzzy paths input. These should both panic most of the time with random input.
+			if path != "m" {
+				assert.Panics(t, func() { generate(hrp, "test", paths, formatter, os.Stdout) }, fmt.Sprintf("Test generate fuzz path did not panic. Path: %s", path))
+				assert.Panics(t, func() {encode(paths, formatter, os.Stdout, hrp, "test")}, fmt.Sprintf("Test encode fuzz path did not panic. Path: %s", path))
+			}
+
+			// Fuzz test generate and encode functions with fuzzy seed. These should usually error with random inputs.
+			if seed != "" && seed != "\r" && seed != "\n" && len(seed) % 4 != 0 {
+				paths := []string{"m/44'/505'/0'/0'/0"}
+				generr := generate(hrp, seed, paths, formatter, os.Stdout)
+				if generr == nil {
+					t.Error(fmt.Sprintf("Test generate fuzz seed did not error. Seed: %s", seed))
+				}
+				encodeErr := encode(paths, formatter, os.Stdout, hrp, seed)
+				if encodeErr == nil {
+					t.Error(fmt.Sprintf("Test encode fuzz seed did not error. Seed: %s", seed))
+				}
+			}
+
+			// Test fuzzy decode with random input.
+			if xkey != "" {
+				decodeErr := decode(hrp, xkey, os.Stdout, formatter)
+				if decodeErr == nil {
+					t.Error(fmt.Sprintf("Test decode fuzz xkey did not error. xkey: %s", xkey))
+				}
+			}
+		}
+	})
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/gogo/protobuf v1.3.2
 	github.com/spf13/cobra v1.5.0
+	github.com/stretchr/testify v1.8.0
 	github.com/tyler-smith/go-bip32 v1.0.0
 	github.com/tyler-smith/go-bip39 v1.1.0
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
@@ -18,6 +19,7 @@ require (
 require (
 	github.com/FactomProject/basen v0.0.0-20150613233007-fe3947df716e // indirect
 	github.com/FactomProject/btcutilecc v0.0.0-20130527213604-d3a63a5752ec // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -33,12 +35,12 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
-	golang.org/x/sys v0.0.0-20220702020025-31831981b65f // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
