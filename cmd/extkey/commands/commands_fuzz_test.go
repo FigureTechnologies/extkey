7aaa5ed4215016bf871840899097ca0cad6b994f
Phil Story <mtps@users.noreply.github.com>
2023-03-06 10:07:33 -0700

    fix lint error

--- cmd/extkey/commands/commands_fuzz_test.go
+++ cmd/extkey/commands/commands_fuzz_test.go
@@ -2,10 +2,11 @@ package commands
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // Golang doesn't support multi fuzz test cases. Aggregated in order to save time fuzzing
@@ -21,11 +22,11 @@ func FuzzCommands(f *testing.F) {
 			// Fuzz test generate and encode functions with fuzzy paths input. These should both panic most of the time with random input.
 			if path != "m" {
 				assert.Panics(t, func() { generate(hrp, "test", paths, formatter, os.Stdout) }, fmt.Sprintf("Test generate fuzz path did not panic. Path: %s", path))
-				assert.Panics(t, func() {encode(paths, formatter, os.Stdout, hrp, "test")}, fmt.Sprintf("Test encode fuzz path did not panic. Path: %s", path))
+				assert.Panics(t, func() { encode(paths, formatter, os.Stdout, hrp, "test") }, fmt.Sprintf("Test encode fuzz path did not panic. Path: %s", path))
 			}
 
 			// Fuzz test generate and encode functions with fuzzy seed. These should usually error with random inputs.
-			if seed != "" && seed != "\r" && seed != "\n" && len(seed) % 4 != 0 {
+			if seed != "" && seed != "\r" && seed != "\n" && len(seed)%4 != 0 {
 				paths := []string{"m/44'/505'/0'/0'/0"}
 				generr := generate(hrp, seed, paths, formatter, os.Stdout)
 				if generr == nil {
